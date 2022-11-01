fa66f2daa48ac8b1cee1c613b1577fb4675ebc15
shkiperoO <44169697+shkiperoO@users.noreply.github.com>
2022-11-01 05:51:45 +0000

    Employees cards

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import femaleProfile from './images/femaleProfile.jpg';
+import maleProfile from './images/maleProfile.jpg';
 
 const Employees = () => {
 
@@ -90,13 +92,28 @@ const [employees,setEmployees] = useState(
 )
   
   return (
-    <main>
-      {
-        employee.map((employee) =>(
-          <p>{employee.fullName}</p>
-        )
-          )
-      }
+    <main className='container'>
+      <div class="row justify-content-center mt-3 mb-3">
+        <div class="col-8">
+            <div class="card-collection">
+              {
+                employees.map((employee) =>(
+                  <div id={employee.id} className="card m-2" style={{ cursor: 'pointer'}}>
+                    {(employee.gender === 'male')?<img src={maleProfile} className="card-img-top"/>:
+                                                  <img src={femaleProfile} className="card-img-top"/>
+                    }
+                    <div className='card-body'>
+                      <h5 className='card-title'>Full Name: {employee.fullName}</h5>
+                      <p className='card-text'>Designation: {employee.designation}</p>
+                    </div>
+                    
+                  </div>
+                )
+                  )
+              }
+            </div>
+          </div>
+      </div>
     </main>
   )
 }
