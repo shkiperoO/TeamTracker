6fa1e74a169ba7c19b8de4757d1dede6efb48a90
shkiperoO <44169697+shkiperoO@users.noreply.github.com>
2022-11-01 04:23:51 +0000

    Employees push

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import Header from './Header';
+import Footer from './Footer';
+import Employees from './Employees';
 
 export default function App() {
   return (
-    <main>
-      Hello world!
-    </main>
+    <div>
+      <Header/>
+      <Employees/> 
+      <Footer/>
+    </div>
   )
 }
